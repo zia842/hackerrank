0c523b5f306ec8a034ddb058345f066e46d1b0dd
Zia Al Khair Mohammed <zia842@gmail.com>
2021-06-24 11:44:04 -0400

    Update NewYearChaos.java

--- NewYearChaos.java
+++ NewYearChaos.java
@@ -7,69 +7,34 @@ public class NewYearChaos {
         
         int[] e = new int[q.length];
         int bribe = 0;
-       /* Map<Integer,Integer> positionMap = new HashMap<Integer,Integer>();
-        for(int i=0;i<e.length;i++) {
-            e[i] = i+1;
-            positionMap.put(e[i],i+1);
-        }
-        boolean flag = false;
-        for(int i=0;i<q.length;i++){
-            if(q[i] != e[i] && q[i] > e[i]){
-                
-                int expectedPos = positionMap.get(q[i]);
-                if(expectedPos-(i+1) > 2)
-                {
-                    System.out.println("Too chaotic");
-                    flag = true;
-                    break;
-                }
-            }
-        }
-        if(!flag)
-        {
-            for(int i=0;i<q.length;i++)
-            {
-            	if(0 == i)
-            		continue;
-            	
-            	if(q[i] != positionMap.get(i+1)) 
-            	{
-            		bribe = bribe + numberOfBribes(q, i);
-            	}
-            }
-        	System.out.println(bribe);
-        }*/
-        
         for(int i=q.length-1;i>=0;i--) {
-        	
-        	if(q[i] != i+1) {
-        		if(((i - 1) >= 0) && q[i - 1] == (i + 1)) {
-        			
-        			bribe = bribe + 1;
-        			int t = q[i-1];
-        			q[i-1] = q[i];
-        			q[i] = t;
-        		}
-        		else if(((i - 2) >= 0) && q[i - 2] == (i + 1))
+            
+            if(q[i] != i+1) {
+                if(((i - 1) >= 0) && q[i - 1] == (i + 1)) {
+                    
+                    bribe = bribe + 1;
+                    int t = q[i-1];
+                    q[i-1] = q[i];
+                    q[i] = t;
+                }
+                else if(((i - 2) >= 0) && q[i - 2] == (i + 1))
                 {
-        			bribe += 2;
+                    bribe += 2;
                     q[i - 2] = q[i - 1];
                     q[i - 1] = q[i];
                     q[i] = i + 1;
                 }
-        		else
+                else
                 {
-        			System.out.println("Too chaotic");
+                    System.out.println("Too chaotic");
                     return;
-                }	
-        		
-        	}
-        	
+                }    
+                
+            }
+            
         }
         
-       for(int i=0;i<q.length;i++) {
-    	   System.out.println(q[i]);
-       }
+        System.out.println(bribe);
 
     }
 	
