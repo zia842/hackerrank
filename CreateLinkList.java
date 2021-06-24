7a57f6e9676eb9313f9b45ea6a123f0ad04b6d5e
Zia Al Khair Mohammed <zia842@gmail.com>
2021-06-23 21:42:02 -0400

    Create CreateLinkList.java

---
+++ CreateLinkList.java
@@ -0,0 +1,20 @@
+
+public class CreateLinkList {
+	
+	public static void main(String[] args) {
+		
+		LinkList l = new LinkList();
+		
+		l.insert(1);
+		l.insert(2);
+		l.insert(3);
+		l.insert(4);
+		l.insert(5);
+		
+		System.out.println("Number of Items in List " + l);
+		
+		l.show();
+
+	}
+
+}
